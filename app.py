d22c8ac3c01bb2d00b1938695fd6e15f93c7c776
FrankyBananas <fbrand@gmail.com>
2020-04-18 15:09:00 +0200

    task 2 app route

--- app.py
+++ app.py
@@ -7,11 +7,25 @@ app = Flask(__name__) # Define the app using Flask
 # app.config["DEBUG"] = True
 
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/task2', methods=['GET', 'POST'])
+def task2():
+    result = ""
+    if request.method == "POST":
+        string1 = None
+        string1 = str(request.form['string1'])
+        if string1 is not None:
+            result = is_json(string1)
+            return render_template("task2_out.html", result=result)
+    return render_template("task2_in.html")
+
+@app.route('/task3', methods=["GET", "POST"])
+def task3():
+    result = ""
+    
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
