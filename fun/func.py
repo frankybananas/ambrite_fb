01d2f7ce3ea41892f5dcd7c82869260358d1a049
FrankyBananas <fbrand@gmail.com>
2020-04-18 14:25:20 +0200

    Task 3&4 Function s started - geo_split

--- fun/func.py
+++ fun/func.py
@@ -11,7 +11,7 @@ merge_out = 'data/merged.json'
 selected_pb = 'data/selected.json' # Output file for last selected items
 
 
-# Task 2 
+# Task 2
 def is_json(filename):
     """
     Check if the specified file is a valid JSON file
@@ -23,3 +23,28 @@ def is_json(filename):
         except JSONDecodeError:
             return False # In case the file is invalid json file
         return True # In case the file is a valid json file
+
+
+# Start Task 3 & Task 4 Functions
+
+# Split Lat and Lon
+def geo_split(file_geo1):
+    """
+    Split the current GEO file LatLon column in two seperate columns
+
+    """
+    with open(file_geo1, 'r') as inp, open(new_geo_out, 'w') as outp:
+        json_decode = json.load(inp)
+        result = []
+        for loca in json_decode:
+            lat_lon = loca['geo'].split(",")
+            result.append({
+                "ipv4": loca["ipv4"],
+                "lat": lat_lon[0],
+                "lon": lat_lon[1]
+            })
+        data = json.dumps(result)
+        outp.write(data)
+    with open(new_geo_out, 'r') as f:
+        geosplit = json.load(f)
+    return geosplit
