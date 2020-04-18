44348f0f6e56b246a3d55e286e16972f6cda513d
FrankyBananas <fbrand@gmail.com>
2020-04-18 14:05:21 +0200

    App main features

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template
+import json
+from json import JSONDecodeError
+from fun.func import is_json, run_task3, run_task4
+
+app = Flask(__name__) # Define the app using Flask
+# app.config["DEBUG"] = True
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
